61170946433435e940cc73114e01ca20ee97c54b
MikaTsur <mikatsur@gmail.com>
2024-05-16 23:46:03 +0300

    perform_login

--- tests/test_github_login.py
+++ tests/test_github_login.py
@@ -6,9 +6,17 @@ def goto_website():
         context = browser.new_context()
         page = context.new_page()
         page.goto('https://github.com/')
-        #page.screenshot(path='example.png')
+        perform_login(page)
         page.pause()
         browser.close()
+
+def perform_login(page):
+    page.get_by_role("link", name="Sign in").click()
+    page.get_by_label("Username or email address").click()
+    page.get_by_label("Username or email address").fill("mikatsurtest")
+    page.get_by_label("Password").click()
+    page.get_by_label("Password").fill("Github-1!")
+    page.get_by_role("button", name="Sign in", exact=True).click()
 
 if __name__ == "__main__":
     goto_website()
