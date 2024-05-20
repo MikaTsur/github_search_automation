cb85d0c50045930f0731fb7f7b796a113e58e627
MikaTsur <mikatsur@gmail.com>
2024-05-20 16:08:40 +0300

    with fixures

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+#automation_tests\tests\conftest.py
+import pytest
+from playwright.sync_api import sync_playwright
+from config.config import Config
+
+@pytest.fixture(scope="function")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        yield browser
+        browser.close()
+
+@pytest.fixture(scope="function")
+def page(browser):
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    context.close()
+
+@pytest.fixture(scope="session")
+def config():
+    return Config('config/config.ini')
--- tests/test_github_search.py
+++ tests/test_github_search.py
@@ -1,11 +1,10 @@
-from config.config import Config
+#automation_tests\tests\test_github_search.py
+import pytest
 from pages.github_search_page import GitHubSearchPage
 
-def test_search_functionality():
-    config_path = 'config/config.ini'
-    config = Config(config_path)
+def test_search_functionality(page, config):
     github_search_url = config.get_github_search_url()
 
-    page = GitHubSearchPage(github_search_url)
-    page.goto()
-    page.search_user("mikatsur")
+    page_obj = GitHubSearchPage(page)
+    page_obj.goto(github_search_url)
+    # Additional test steps if needed
