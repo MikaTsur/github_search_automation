41fa20cafebed6488664e7655efac747e3f73fda
MikaTsur <mikatsur@gmail.com>
2024-05-26 09:52:45 +0300

    'more nice things'

--- tests/test_dolar_rate.py
+++ tests/test_dolar_rate.py
@@ -7,26 +7,33 @@ import logging
 
 # Test case using pytest
 def test_login_functionality():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        response = requests.get('https://www.themarker.com/')
-        if response.status_code == 200:
-            logging.info('response 200')
-            soup = BeautifulSoup(response.content, 'html.parser')
-            target_span = soup.find('span', text="דולר-שקל רציף")
-            if target_span:
-                value_span = target_span.find_next('span')
-                if value_span:
-                    value = value_span.text
-                    print(value)
-                    logging.info(f'value is {value}')
-                else:
-                    print("Value span not found.")
-            else:
-                print("Target span not found.")
-        else:
-            print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    # with sync_playwright() as p:
+    #     browser = p.chromium.launch(headless=False)
+    #     context = browser.new_context()
+    #page = context.new_page()
+    response = requests.get('https://www.themarker.com/')
+    if response.status_code == 200:
+        logging.info('response 200')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        target_span = soup.find('span', text="דולר-שקל רציף")
+        if target_span:
+            value_span = target_span.find_next('span')
+            if value_span:
+                value = value_span.text
+                print(value)
+                logging.info(f'value is {value}')
+    #browser.close()
 
-        browser.close()
+
+def test_api_only():
+    logging.info('EEEEEEEEEEEEEEEEEEEEEEEEEEEE')
+    response = requests.get('https://www.themarker.com/')
+    if response.status_code == 200:
+        logging.info('response 200')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        target_span = soup.find('span', text="דולר-שקל רציף")
+        if target_span:
+            value_span = target_span.find_next('span')
+            if value_span:
+                value = value_span.text
+                logging.info(f'value is {value}')
